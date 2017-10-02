650dedbef6a196b7728964a0ad16b3592be9aae6
Ben Burkert <ben@benburkert.com>
2017-10-01 18:27:29 -0700

    dns: optimize decompression
    name                                      old time/op    new time/op    delta
    MessageUnpack/small-message-8               1.05µs ± 2%    0.91µs ± 1%  -13.40%  (p=0.000 n=10+9)
    MessageUnpack/large-message-8               6.51µs ± 2%    4.21µs ± 1%  -35.45%  (p=0.000 n=9+10)
    MessageDecompress/small-message-8           1.24µs ±10%    1.04µs ± 1%  -15.74%  (p=0.000 n=10+10)
    MessageDecompress/large-message-8           7.76µs ± 4%    5.05µs ± 1%  -35.00%  (p=0.000 n=10+8)
    
    name                                      old speed      new speed      delta
    MessageUnpack/small-message-8              105MB/s ± 2%   121MB/s ± 1%  +15.42%  (p=0.000 n=10+9)
    MessageUnpack/large-message-8              103MB/s ± 2%   159MB/s ± 1%  +54.91%  (p=0.000 n=9+10)
    MessageDecompress/small-message-8         62.3MB/s ± 9%  73.8MB/s ± 1%  +18.35%  (p=0.000 n=10+10)
    MessageDecompress/large-message-8         46.5MB/s ± 4%  71.5MB/s ± 1%  +53.78%  (p=0.000 n=10+8)
    
    name                                      old alloc/op   new alloc/op   delta
    MessageUnpack/small-message-8                 480B ± 0%      592B ± 0%  +23.33%  (p=0.000 n=10+10)
    MessageUnpack/large-message-8               2.67kB ± 0%    2.79kB ± 0%   +4.49%  (p=0.000 n=10+10)
    MessageDecompress/small-message-8             512B ± 0%      640B ± 0%  +25.00%  (p=0.000 n=10+10)
    MessageDecompress/large-message-8           2.98kB ± 0%    2.97kB ± 0%   -0.27%  (p=0.000 n=10+10)
    
    name                                      old allocs/op  new allocs/op  delta
    MessageUnpack/large-message-8                  112 ± 0%        78 ± 0%  -30.36%  (p=0.000 n=10+10)
    MessageDecompress/large-message-8              132 ± 0%        98 ± 0%  -25.76%  (p=0.000 n=10+10)

--- compression.go
+++ compression.go
@@ -94,30 +94,37 @@ func (c compressor) Pack(b []byte, fqdn string) ([]byte, error) {
 type decompressor []byte
 
 func (d decompressor) Unpack(b []byte) (string, []byte, error) {
-	return d.unpack(b, nil)
+	name, b, err := d.unpack(make([]byte, 0, 32), b, nil)
+	if err != nil {
+		return "", nil, err
+	}
+	return string(name), b, nil
 }
 
-func (d decompressor) unpack(b []byte, visited []int) (string, []byte, error) {
+func (d decompressor) unpack(name, b []byte, visited []int) ([]byte, []byte, error) {
 	lenb := len(b)
 	if lenb == 0 {
-		return "", nil, errBaseLen
+		return nil, nil, errBaseLen
 	}
 	if b[0] == 0x00 {
-		return ".", b[1:], nil
+		if len(name) == 0 {
+			return append(name, '.'), b[1:], nil
+		}
+		return name, b[1:], nil
 	}
 	if lenb < 2 {
-		return "", nil, errBaseLen
+		return nil, nil, errBaseLen
 	}
 
 	if isPointer(b[0]) {
 		if d == nil {
-			return "", nil, errInvalidPtr
+			return nil, nil, errBaseLen
 		}
 
 		ptr := nbo.Uint16(b[:2])
-		name, err := d.deref(ptr, visited)
+		name, err := d.deref(name, ptr, visited)
 		if err != nil {
-			return "", nil, err
+			return nil, nil, err
 		}
 
 		return name, b[2:], nil
@@ -126,38 +133,32 @@ func (d decompressor) unpack(b []byte, visited []int) (string, []byte, error) {
 	lenl, b := int(b[0]), b[1:]
 
 	if len(b) < lenl {
-		return "", nil, errCalcLen
+		return nil, nil, errCalcLen
 	}
 
-	label := string(b[:lenl])
+	name = append(name, b[:lenl]...)
+	name = append(name, '.')
 
-	suffix, b, err := d.unpack(b[lenl:], visited)
-	if err != nil {
-		return "", nil, err
-	}
-	if suffix == "." {
-		return label + ".", b, nil
-	}
-	return label + "." + suffix, b, nil
+	return d.unpack(name, b[lenl:], visited)
 }
 
-func (d decompressor) deref(ptr uint16, visited []int) (string, error) {
+func (d decompressor) deref(name []byte, ptr uint16, visited []int) ([]byte, error) {
 	idx := int(ptr & 0x3FFF)
 	if len(d) < idx {
-		return "", errInvalidPtr
+		return nil, errInvalidPtr
 	}
 
 	if isPointer(d[idx]) {
-		return "", errInvalidPtr
+		return nil, errInvalidPtr
 	}
 
 	for _, v := range visited {
 		if idx == v {
-			return "", errPtrCycle
+			return nil, errPtrCycle
 		}
 	}
 
-	name, _, err := d.unpack(d[idx:], append(visited, idx))
+	name, _, err := d.unpack(name, d[idx:], append(visited, idx))
 	return name, err
 }
 
